397d61aab914b96a26b392b7006d5a5111c38919
yangwonpark <ailey@nate.com>
2021-03-28 14:40:49 +0900

    3. 산술 & 할당 연산자

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-import getType from './getType.js'
+// 산술 연산자(arithmetic operator)
 
-console.log(typeof 'Hello World!');
-console.log(typeof 123);
-console.log(typeof true);
-console.log(typeof undefined);
-console.log(typeof null);
-console.log(typeof {});
-console.log(typeof []);
+console.log(1 + 2);
+console.log(5 - 7);
+console.log(3 * 4);
+console.log(10 / 2);
+console.log(7 % 5);
 
 
 
-console.log(getType(123));
-console.log(getType(false));
-console.log(getType(null));
-console.log(getType({}));
-console.log(getType([]));
+// 할당 연산자(assignment operator)
+
+let a = 2;
+// a = a + 1;
+a += 1;
+
+console.log(a);
